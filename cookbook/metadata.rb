6198aaa29d8fd59ee77ea0f3cb19c838bb7ed030
Jonathan Metzger <9845417+jrmetzger@users.noreply.github.com>
2025-03-12 03:29:37 -0400

    2 audit controls test

---
+++ cookbook/metadata.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+name 'cookbook'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures applications'
+version '0.1.0'
+chef_version '>= 16.0'
